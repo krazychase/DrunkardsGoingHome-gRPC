dcff951c3b52d98aee9a986e3f52ee2097241d2e
Chase Brown <krazychase@gmail.com>
2023-01-01 21:27:24 -0700

    Added gRPC functions

--- GoHoServer.py
+++ GoHoServer.py
@@ -8,31 +8,93 @@ import protobufs.goho_pb2 as pb2
 import protobufs.goho_pb2_grpc as pb2_grpc
 
 class User():
-    def __init__(self):
-        pass
+    '''
+    Contains User information
+    '''
+    def __init__(self, userid=None, username=None, password=None, homeLocation=None):
+        self.userid = userid
+        self.username = username
+        self.password = password
+        self.homeLocation = homeLocation
 
 class Ride():
+    '''
+    Contains Ride Information
+    '''
+    def __init__(self, rideid=None, rider=None, driver=None, destination=None, location=None, 
+                time=None, status=None):
+        self.rideid = rideid
+        self.rider = rider
+        self.driver = driver
+        self.destination = destination
+        self.location = location
+        self.time = time
+        self.status = status
+
+class GoHoService(pb2_grpc.GoHoServiceServicer):
+    '''
+    gRPC Service
+    '''
     def __init__(self):
         pass
 
-class GoHoService():
-    def __init__(self):
-        pass
+    def GetUser(self, request, context):
+        '''
+        Returns User information
+        '''
+        userID = request.userid
+        # Get user info from DB
+        user = User()
+        response = {'userid':user.userid, 'username':user.username, 'password':user.password, 
+                    'home_location':user.homeLocation}
 
-    def GetUser():
-        pass
+        return pb2.Response(**response)
 
-    def AddUser():
-        pass
+    def AddUser(self, request, context):
+        '''
+        Adds user to DB
+        Returns confirmation
+        '''
+        user = User(request.userid, request.username, request.password, request.home_location)
+        # Add user to DB
+        response = {'response':f'User {user.userid}', 'code':0}
 
-    def GetRides():
-        pass
+        return pb2.Response(**response)
 
-    def AddRide():
-        pass
+    def GetRides(self, request, context):
+        '''
+        Returns ride information
+        '''
+        rideid = request.rideid
+        # Get ride info from DB
+        ride = Ride()
+        response = {'rideid':ride.rideid, 'rider':ride.rider, 'driver':ride.driver, 
+                    'destination':ride.destination, 'location':ride.location, 'time':ride.time, 
+                    'status':ride.status}
 
-    def UpdateRide():
-        pass
+        return pb2.Response(**response)
+
+    def AddRide(self, request, context):
+        '''
+        Adds ride to DB
+        Returns confirmation
+        '''
+        ride = Ride(request.rideid, request.rider, request.driver, request.destination, 
+                    request.location, request.time, request.status)
+        # Add ride to DB
+        response = {'response':f'Ride {ride.rideid}', 'code':0}
+
+        return pb2.Response(**response)
+
+    def UpdateRide(self, request, context):
+        '''
+        Updates ride on DB
+        Returns Confirmation
+        '''
+        ride = Ride(request.rideid, request.rider, request.driver, request.destination, 
+                    request.location, request.time, request.status)
+        # Update ride in DB
+        response = {'response':f'Ride {ride.rideid}', 'code':0}
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
